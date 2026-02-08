71766d0fb6515525639ce9ef03f354859cdbb5c6
Kyle Espulgar <espulgarkyle25@gmail.com>
2026-02-09 04:52:53 +0800

    enchance styling and add about and projects section

--- js/script.js
+++ js/script.js
@@ -14,3 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 20) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
